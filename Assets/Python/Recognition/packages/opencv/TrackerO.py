905cecf1c817e4e6473d4f25d3aebeffbd8967a6
Singhto69 <Timmerskold@hotmail.com>
2022-02-07 22:19:17 +0100

    OOP version , shiny happy people

--- Assets/Python/Recognition/packages/opencv/TrackerO.py
+++ Assets/Python/Recognition/packages/opencv/TrackerO.py
@@ -1,21 +1,31 @@
 import cv2
 
+
 class TrackerO:
-    def __init__ (self):
+    def __init__(self):
         self.tracker = None
+        self.trackers = []
         self.init = False
         self.trackOk = None
         self.trackBox = None
 
-    def setTrackBox(self,array):
+    def setTrackBox(self, array):
         self.trackBox = array
-    def setCSRT(self):
-        self.tracker = cv2.TrackerTLD_create()
-    def setMOSSE(self):
-        self.tracker = cv2.TrackerMOSSE_create()
-    def setTLD(self):
-        self.tracker = cv2.TrackerCSRT_create()
 
+    def getTrackBox(self):
+        return self.trackBox
 
+    def crateTLD(self):
+        self.trackers.append(cv2.TrackerTLD_create())
 
+    def createMOSSE(self):
+        self.trackers.append(cv2.TrackerMOSSE_create())
 
+    def createCSRT(self):
+        self.trackers.append(cv2.TrackerCSRT_create())
+
+    def getTracker(self):
+        return self.trackers[-1]
+
+    def removeFirstTracker(self):
+        self.trackers.remove(self.trackers[0])
